fd69ededded9143416bb423cdbe0c480afa072d8
RandomYang <decoderyang@gmail.com>
2025-01-22 17:42:08 +0800

    Initial Commit

---
+++ Metal_LoopBlinn/Metal_LoopBlinnApp.swift
@@ -0,0 +1,17 @@
+//
+//  Metal_LoopBlinnApp.swift
+//  Metal_LoopBlinn
+//
+//  Created by randomyang on 2025/1/22.
+//
+
+import SwiftUI
+
+@main
+struct Metal_LoopBlinnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Metal_LoopBlinnTests/Metal_LoopBlinnTests.swift
@@ -0,0 +1,17 @@
+//
+//  Metal_LoopBlinnTests.swift
+//  Metal_LoopBlinnTests
+//
+//  Created by randomyang on 2025/1/22.
+//
+
+import Testing
+@testable import Metal_LoopBlinn
+
+struct Metal_LoopBlinnTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
